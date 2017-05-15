a8933fc4249e379c3ee903f763bde10e17c19667
Luis Carlos Gomez <lgomezca1327@universidadean.edu.co>
2017-05-15 11:40:30 -0500

    Actualizacion de paginas principal , contacto y clientes, se adiciona validacion Javascript a formulacion de contacto

--- js/menu.js
+++ js/menu.js
@@ -1,3 +1,4 @@
+/*Función para cambiar estilo y presentacion de menu al reducir tamaño de pantalla*/
 var btnMenu = document.getElementById('btn-menu');
 var nav = document.getElementById('nav');
 
---
+++ js/validacion-formulario.js
@@ -0,0 +1,25 @@
+/*-----------------Funcion para  Validar campos vacios-----------------*/
+/*jslint devel: true */
+/*jslint browser:true */
+function validarCamposFormulario() {
+    "use strict";
+    var j = 0;
+    var error = false;
+    var msj = "Complete los siguientes campos\n\r\n";
+    var elementos = document.forms[0].elements;
+    /*Recorremos los elementos del formulario
+       validando si su longitu es igual a Cero*/
+    while (j < elementos.length) {
+        if (elementos[j].value.length === 0 && elementos[j].id !== "submit") {
+            msj += elementos[j].id + "\n";
+            error = true;
+        }
+        j = j + 1;
+    }
+    if (error === true) {
+        alert(msj);
+        return false;
+    }
+    alert("Gracias " + document.forms[0].elements[0].value + " por registrarse en nuestra pagina ")
+    return;
+}
