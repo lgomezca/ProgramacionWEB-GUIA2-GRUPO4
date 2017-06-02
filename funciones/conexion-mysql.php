2511054c8a609a89de7f52ccb98220fa12c6ce23
DALC1314 <dlatorre8923@universidadean.edu.co>
2017-06-02 16:51:04 -0500

    BD_Online_update_string

--- funciones/conexion-mysql.php
+++ funciones/conexion-mysql.php
@@ -1,6 +1,6 @@
 <?
 //Creamos la conexion con la Base de datos
-$conexion= mysql_connect("localhost", "root", "adminmysql")or die("fallo en conexion con BD");
-mysql_select_db("hdsolutions") or die('BD no encontrada');    
+$conexion= mysql_connect("sql10.freemysqlhosting.net", "sql10177996", "S8SfTh2IYv")or die("fallo en conexion con BD");
+mysql_select_db("sql10177996") or die('BD no encontrada');    
 
 ?>
